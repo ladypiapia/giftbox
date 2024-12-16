d237a97ce2391ddf081105fa842824295724148e
eugenechantk <me@eugenechantk.me>
2024-12-16 21:01:12 +0800

    directly create a new letter when landing on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function LandingPage() {
-  return <div>Main page</div>;
+import { createId } from "@paralleldrive/cuid2";
+import { redirect } from "next/navigation";
+
+export default async function LandingPage() {
+  // Generate a new CUID
+  const id = createId();
+  // Redirect to the new page
+  redirect(`/${id}`);
 }
